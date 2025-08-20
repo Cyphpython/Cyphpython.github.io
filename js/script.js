2e684af6908b87b4b62136311a4b37a2ec17885d
Elden Modder <90405810+Cyphpython@users.noreply.github.com>
2025-08-20 20:30:19 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Active le scroll fluide quand on clique sur un lien de la navbar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
